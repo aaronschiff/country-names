754db0b8de5829d851035215719fe7836f72885e
Aaron Schiff <aaron@schiff.co.nz>
2015-08-26 17:37:20 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Country disambiguation
+
+These scripts assist with the construction of a list to disambiguate the names of countries. The list of ISO 3166 countries from Wikipedia is used as the list of "standard" country names (https://en.wikipedia.org/wiki/ISO_3166). 
+
+The script get3166.py scrapes the list of ISO 3166 country names and other information from Wikipedia and saves it in countries.csv.
+
+The script disambiguate.py generates a lookup table of alternative names for each of the ISO 3166 country names. It does this by parsing the list of transitive redirects on Wikipedia constructed by the DBpedia project (http://downloads.dbpedia.org/2015-04/core-i18n/en/transitive-redirects_en.nt.bz2). 
+
+The end result is a CSV file with two columns: alternative country names, and standardised (ISO 3166) names. 
---
+++ get3166.py
@@ -0,0 +1,40 @@
+from bs4 import BeautifulSoup
+import urllib2
+import string
+import csv
+
+url = "https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2"
+page = urllib2.urlopen(url)
+
+soup = BeautifulSoup(page.read(), 'html.parser')
+
+countriesTable = None 	# Will store the table after we find it
+maxRows = 0				# For finding table with the most rows
+
+for table in soup.findAll('table', {'class' : 'wikitable'}):
+	rows = table.findAll('tr')
+	
+	# The table we want is the longest one on the page
+	if len(rows) > maxRows:
+		maxRows = len(rows)
+		countriesTable = table
+		
+# Extract data from the table of countries
+report = []
+for row in countriesTable.findAll('tr'):
+	items = []
+	for td in row.findAll('td'): 
+		items.append(td.get_text().encode('utf8'))
+		for link in td.findAll('a'):
+			items.append(link.get('href'))
+	report.append(items)
+	
+# header = ['Code', 'Country name', 'Year', 'ccTLD', 'ISO 3166-2', 'Notes']
+
+with open('countries.csv', 'wb') as f:
+	writer = csv.writer(f)
+	# writer.writerow(header)
+	for row in report:
+		writer.writerow(row[0:9])		# Write first 9 columns only
+#	writer.writerows(row for row in report if row)
+	
